b1cb616cddba545d14b8c224db2664178e99b050
Stephen Hibit <sdhibit@gmail.com>
2013-07-28 10:26:45 -0400

    Fixes compilation error.
    value_for_platform requires default parameter to be array

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,13 +21,13 @@
 # * vim-minimal gives you /bin/vi
 # * vim-enhanced gives you /usr/bin/vim
 vim_base_pkgs = value_for_platform(
-  ["ubuntu", "debian", "arch"] => { "default" => "vim" },
+  ["ubuntu", "debian", "arch"] => { "default" => ["vim"] },
   ["redhat", "centos", "fedora", "scientific"] => { "default" => ["vim-minimal","vim-enhanced"] },
-  ["freebsd"] => { "default" => "vim-lite"},
+  ["freebsd"] => { "default" => ["vim-lite"]},
   "default" => ["vim"]
 )
 
-*vim_base_pkgs.each do |vim_base_pkg|
+vim_base_pkgs.each do |vim_base_pkg|
   package vim_base_pkg
 end
 
